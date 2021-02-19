399987cd17e71e5b53f224016b9a38b9c5c58d0f
putrirenan <putrirenan@gmail.com>
2021-02-19 12:50:45 +0700

    menyelesaikan video pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,67 @@
+<?php
+/*
+    Renandra Rahadian Putri
+    203040153
+    https://github.com/Putrirenan/pw2021_203040153
+    Pertemuan 2 - 8 Februari 2021
+    Mempelajari mengenai Sintaks PHP
+*/
+?>
+
+<?php 
+
+// Pertemuan 2 - PHP Dasar 
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// print_r
+// var_dump = untuk melihat isi dari vairable 
+
+// Penulisan Sintaks PHP
+// 1. PHP didalam HTML
+// 2. HTML didalam PHP
+
+// Variable dan Tipe Data
+// Variable
+// tidak boleh diawali dengan angka, tetapi boleh mengandung angka
+// $nama = "Putrirenan";
+// echo "Halo, nama saya $nama"
+
+
+// Operator
+// aritmatika + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// penggabung string / concantenation / concat
+// .
+// $nama_depan = "Renandra";
+// $nama_belakang = "putri";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assignment
+// // =, +=, -=, *=, /=, %=, .=
+// $x = 1;
+// $x += 5;
+// echo $x;
+// $nama = "Renan";
+// $nama .= " ";
+// $nama .= "Putri";
+// echo $nama;
+
+// Perbandingan <, >, <=, >=, ==, !=
+// var_dump(1 == "1");
+
+// // identitas ===, !==
+// var_dump(1 === "1");
+
+// logika
+// // &&, ||, ! 
+// $x = 30;
+// var_dump($x <20 || $x % 2 == 0);
+
+
+
+?>
